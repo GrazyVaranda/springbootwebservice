d6c6eec620f04f551e2a28d7e144403d0ae32d30
CrazyVeranda <150302583+GrazyVaranda@users.noreply.github.com>
2024-06-11 19:59:21 -0300

    feat: update LocalizacaoService.java

--- example/src/main/java/br/com/example/LocalizacaoService.java
+++ example/src/main/java/br/com/example/LocalizacaoService.java
@@ -1,15 +1,37 @@
-package br.com.example;
 
-      import org.springframework.web.bind.annotation.GetMapping;
-	  import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-	  @RestController
-	  public class LocalizacaoService {
 
-	   @GetMapping("obtemlocal") public String obtemlocalizacao() {
-	   return "São Miguel Paulista";
+@RestController
+public class LocalizacaoService {
 
-	  }
+    @GetMapping("/obtemlocal")
+    public String obtemLocalizacao() {
+        return "São Miguel Paulista";
+    }
 
-	 }
+    @GetMapping("/obtemlocal")
+    public String obtemHoraAtual() {
+        return LocalDateTime.now().toString();
+    
+    
+   }
+    @GetMapping("/obtelocal")
+    public String obtemCoordenadas() {
+        
+        return "Latitude: xx.xxxx, Longitude: yy.yyyy";
+    }
 
+    @GetMapping("/obtemlocal")
+    public String obtemEndereco() {
+        
+        return "Endereço: Rua abc , Cidade São Paulo, Estado São Paulo";
+    }
+
+    @GetMapping("/obtemlocal")
+    public String obtemPais() {
+       
+        return "País: Brasil, Capital: São Paulo, População: xxx milhões";
+    }
+}
