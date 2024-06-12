6285eb1821db9f0781145e0af92087004bbb1b15
CrazyVeranda <150302583+GrazyVaranda@users.noreply.github.com>
2024-06-12 01:11:03 -0300

    feat:update LocalizacaoService.java

--- example/src/main/java/br/com/example/LocalizacaoService.java
+++ example/src/main/java/br/com/example/LocalizacaoService.java
@@ -1,9 +1,11 @@
 package br.com.example;
+
 import java.time.LocalDateTime;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class LocalizacaoService {
@@ -16,24 +18,22 @@ public class LocalizacaoService {
     @GetMapping("/obtemhora")
     public String obtemhora() {
         return LocalDateTime.now().toString();
-    
-    
-   }
-    @GetMapping("/obtemcoordenadas")
-    public String obtemCoordenadas() {
-        
-        return "Latitude: A, Longitude: B";
     }
 
-    @GetMapping("/obtemendereco")
-    public String obtemendereco() {
-        
-        return "Endereço: Rua abc n° xxx, Cidade São Paulo, Estado São Paulo";
+    @GetMapping("/obtemcoordenadas")
+    public String obtemCoordenadas(@RequestParam(value = "latitude", defaultValue = "0.0") String latitude,
+                                   @RequestParam(value = "longitude", defaultValue = "0.0") String longitude) {
+        return "Latitude: " + latitude + ", Longitude: " + longitude;
+    }
+
+    @GetMapping("/obtemendereco/{rua}/{numero}")
+    public String obtemendereco(@PathVariable String rua, @PathVariable String numero) {
+        return "Endereço: Rua " + rua + " n° " + numero + ", Cidade São Paulo, Estado São Paulo";
     }
 
     @GetMapping("/obtempais")
-    public String obtempais() {
-       
-        return "País: Brasil, População: 215,3 milhões";
+    public String obtempais(@RequestParam(value = "nome", defaultValue = "Brasil") String nome,
+                            @RequestParam(value = "populacao", defaultValue = "215,3 milhões") String populacao) {
+        return "País: " + nome + ", População: " + populacao;
     }
 }
